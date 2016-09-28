31a6af07c6ca97f11247223dc78323a977346a41
Ednas <edna@edna.tech>
2016-09-28 00:18:09 -0400

    set up express and node server

---
+++ server.js
@@ -0,0 +1,18 @@
+//set up dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//creates express server and sets up a port
+var app = express(); 
+var port = process.env.PORT || 3000; 
+
+//Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+//Listening to the port that was set up
+app.listen(port, () => console.log("Listening on port %s", port));
