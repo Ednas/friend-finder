360e1d6428d06d2f0df9a32fcb6010a63236898e
Ednas <edna@edna.tech>
2016-10-08 23:33:43 -0400

    Added stylesheet and logo to html, fixed routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = function(app){
 
 //GET request 
-	app.get('survey', function(req, res){
+	app.get('/survey', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
 
